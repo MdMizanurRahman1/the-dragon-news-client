d851368c48aaf54350da2fd19d80466661c03c01
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-04-29 20:47:57 +0300

    rating changed

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author, rating, total_view } = news;
@@ -28,15 +29,9 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
             </Card.Body>
             <Card.Footer className="text-muted d-flex align-items-center ">
-                <div className='flex-grow-1'>
-                    <Rating
-                        placeholderRating={rating.number}
-                        readonly
-                        emptySymbol={<FaRegStar></FaRegStar>}
-                        placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                        fullSymbol={<FaStar></FaStar>}
-                    ></Rating>
-                    <span> {rating?.number}</span>
+                <div className='flex-grow-1 d-flex align-items-center'>
+                    <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+                    <span className='ms-2'> {rating?.number}</span>
                 </div>
                 <div>
                     <FaEye /> {total_view}
